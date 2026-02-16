1836185e92c4e3f27748a29c16751f202e06dbb6
Sumit Kumar Das <skdas5405@gmail.com>
2026-02-16 21:53:15 +0530

    Fix: Add @stack('scripts') to category pages to render Vue component templates

--- packages/Webkul/Shop/src/Resources/views/categories/dates.blade.php
+++ packages/Webkul/Shop/src/Resources/views/categories/dates.blade.php
@@ -87,6 +87,8 @@
     </script>
     @endpushOnce
     
+    @stack('scripts')
+    
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
     <script src="{{ asset('thf-assets/js/home.js') }}"></script>
